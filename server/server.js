2d302933f4ed4737d50b01ddf5a0cce45101013f
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2024-09-30 08:53:57 +0530

    Correct Port

--- server/server.js
+++ server/server.js
@@ -41,5 +41,5 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
